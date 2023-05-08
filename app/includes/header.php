183ab4f400b8e1daedc359707b4ad726feac21eb
unknown <ivan.200512340@gmail.com>
2023-05-08 17:41:42 +0300

    fix

--- app/includes/header.php
+++ app/includes/header.php
@@ -1,9 +1,15 @@
 <div class="header">
   <div class="header-menu">
     <div class="header-menu__wrapper">
+      
       <a class="header-menu__item" href="../suppliers/index">Поставщики</a>
       <a class="header-menu__item" href="../invoices/index">Накладные</a>
-      <a class="header-menu__item" href="../products/index">Товары</a>
+      <div class="header-menu__item" >
+        <a href="../products/index" class="header-menu__item__link">Товары</a>
+        <div class="header-menu__item-sublock">
+          <a href="../products/add" class="header-menu__item-sublock__item">Добавить товар</a>
+        </div>
+      </div>
       <a class="header-menu__item" href="../orders/index">Заказы</a>
       <a class="header-menu__item" href="../orders/ones">Заказы детально</a>
     </div>
--- app/views/template_view.php
+++ app/views/template_view.php
@@ -11,7 +11,7 @@
 
 </head>
 
-<body>
+<body class="styled-scrollbars">
 	<?php include 'app/includes/header.php'; ?>
 	<div id="content-wrap">
 		<div class="content">
