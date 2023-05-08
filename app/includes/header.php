b545484101b57dca41e846e0149d717b4966c7f9
unknown <ivan.200512340@gmail.com>
2023-05-08 19:07:21 +0300

    smaller fix

--- app/includes/header.php
+++ app/includes/header.php
@@ -1,17 +1,28 @@
 <div class="header">
   <div class="header-menu">
     <div class="header-menu__wrapper">
-      
-      <a class="header-menu__item" href="../suppliers/index">Поставщики</a>
-      <a class="header-menu__item" href="../invoices/index">Накладные</a>
+      <div class="header-menu__item" >
+        <a href="../suppliers/index" class="header-menu__item__link">Поставщики</a>
+        <div class="header-menu__item-sublock">
+        </div>
+      </div>
+      <div class="header-menu__item" >
+        <a href="../invoices/index" class="header-menu__item__link">Накладные</a>
+        <div class="header-menu__item-sublock">
+        </div>
+      </div>
       <div class="header-menu__item" >
         <a href="../products/index" class="header-menu__item__link">Товары</a>
         <div class="header-menu__item-sublock">
           <a href="../products/add" class="header-menu__item-sublock__item">Добавить товар</a>
         </div>
       </div>
-      <a class="header-menu__item" href="../orders/index">Заказы</a>
-      <a class="header-menu__item" href="../orders/ones">Заказы детально</a>
+      <div class="header-menu__item" >
+        <a href="../orders/index" class="header-menu__item__link">Заказы</a>
+        <div class="header-menu__item-sublock">
+          <a href="../orders/ones" class="header-menu__item-sublock__item">Заказы детально</a>
+        </div>
+      </div>
     </div>
 
   </div>
